e42aad5c911912103ecdb87f8f53142b34838029
luk-kop <luk.kopicki@gmail.com>
2022-02-06 16:25:33 +0100

    Added DynamoDB resource.

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.main_region
-}
-
 resource "aws_s3_bucket" "terraform_s3_state_bucket" {
   bucket_prefix = var.bucket_name_prefix
   acl           = "private"
@@ -21,6 +17,26 @@ resource "aws_s3_bucket" "terraform_s3_state_bucket" {
 
   tags = {
     Tool        = "terraform"
-    Environment = "dev"
+    Environment = var.tag_environment
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_s3_state_bucket_public_access" {
+  bucket                  = aws_s3_bucket.terraform_s3_state_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_dynamodb_locks" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,19 @@
-variable "main_region" {
-  type = string
-}
-
 variable "bucket_name_prefix" {
   type    = string
-  default = "terraform-backend-"
+  default = "terraform-state-dev-"
+}
+
+variable "dynamodb_table_name" {
+  type    = string
+  default = "terraform-lock"
 }
 
 variable "bucket_versioning" {
   type    = bool
   default = true
+}
+
+variable "tag_environment" {
+  type    = string
+  default = "dev"
 }
