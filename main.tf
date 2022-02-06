1411faa7f7aa28603c468e7dd22ad42e66c8e285
luk-kop <luk.kopicki@gmail.com>
2022-02-06 14:13:09 +0100

    Added .gitignore file.

--- main.tf
+++ main.tf
@@ -2,12 +2,21 @@ provider "aws" {
   region = var.main_region
 }
 
-resource "aws_s3_bucket" "s3_state_bucket" {
+resource "aws_s3_bucket" "terraform_s3_state_bucket" {
   bucket_prefix = var.bucket_name_prefix
   acl           = "private"
+  force_destroy = true
 
   versioning {
     enabled = var.bucket_versioning
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 
   tags = {
