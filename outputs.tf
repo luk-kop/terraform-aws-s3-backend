cc9140b8e675c25152b5a11484cfb64532555584
luk-kop <luk.kopicki@gmail.com>
2022-02-07 14:45:15 +0100

    Updated outputs.tf file.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.terraform_s3_state_bucket.arn
-  description = "The ARN of the S3 bucket"
+  description = "The ARN of S3 bucket in which the state file will be stored."
+}
+
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.terraform_s3_state_bucket.id
+  description = "The name of S3 bucket in which the state file will be stored."
 }
 
 output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_dynamodb_locks.name
-  description = "The name of the DynamoDB table"
+  description = "The name of DynamoDB table used for state locking and consistency."
+}
+
+output "iam_role_arn" {
+  value       = aws_iam_role.terraform_backend_iam_role.arn
+  description = "The IAM Role ARN to assume."
 }
