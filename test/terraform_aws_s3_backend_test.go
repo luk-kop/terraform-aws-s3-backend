3e4ace47f48c25bfa0374e162f0ae7647a202c38
luk-kop <lukasz.kopicki@sysdogs.com>
2022-05-15 09:26:35 +0200

    Fixed terratest

--- test/terraform_aws_s3_backend_test.go
+++ test/terraform_aws_s3_backend_test.go
@@ -1,17 +1,17 @@
 package test
 
 import (
-    "fmt"
+	"fmt"
 	"strings"
-    "testing"
+	"testing"
 
-    "github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
-    "github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTerrafromS3Backend(t *testing.T){
+func TestTerrafromS3Backend(t *testing.T) {
 	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 	expectedStateBucketNamePrefix := "terratest-aws-s3-backend"
 	expectedLogsBucketNamePrefix := "terratest-aws-s3-backend-logs"
@@ -20,41 +20,43 @@ func TestTerrafromS3Backend(t *testing.T){
 	expectedDynamoDbTableName := fmt.Sprintf("terratest-state-lock-%s", strings.ToLower(random.UniqueId()))
 	expectedTags := map[string]string{
 		"Environment": expectedEnvironmentTag,
-		"Tool": expectedToolTag,
+		"Tool":        expectedToolTag,
 	}
 
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-        TerraformDir: "./fixtures",
-        
-        // Variables to pass to the Terraform code using -var options
-        Vars: map[string]interface{}{
-            "bucket_name_prefix": expectedStateBucketNamePrefix,
-			"dynamodb_table_name": expectedDynamoDbTableName,
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./fixtures",
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"bucket_name_prefix":        expectedStateBucketNamePrefix,
+			"dynamodb_table_name":       expectedDynamoDbTableName,
 			"bucket_versioning_enabled": "true",
-			"bucket_objects_deletion": "false",
+			"bucket_objects_deletion":   "false",
 			"tags": map[string]string{
 				"Environment": expectedEnvironmentTag,
-				"Tool": expectedToolTag,
+				"Tool":        expectedToolTag,
 			},
-        },
+		},
 		EnvVars: map[string]string{
 			"AWS_REGION": awsRegion,
 		},
-    })
-    
-    defer terraform.Destroy(t, terraformOptions)
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
-    
+
 	// Outputs
 	stateBucketName := terraform.Output(t, terraformOptions, "state_bucket_name")
 	logsBucketName := terraform.Output(t, terraformOptions, "logs_bucket_name")
 
 	// Verify state bucket name
-	strings.HasPrefix(stateBucketName, expectedStateBucketNamePrefix)
+	stateBucketPrefixed := strings.HasPrefix(stateBucketName, expectedStateBucketNamePrefix)
+	assert.Equal(t, true, stateBucketPrefixed)
 
 	// Verify logs bucket name
-	strings.HasPrefix(logsBucketName, expectedLogsBucketNamePrefix)
+	logsBucketPrefixed := strings.HasPrefix(logsBucketName, expectedLogsBucketNamePrefix)
+	assert.Equal(t, true, logsBucketPrefixed)
 
 	// Verify that state bucket has versioning enabled
 	actualStatus := aws.GetS3BucketVersioning(t, awsRegion, stateBucketName)
